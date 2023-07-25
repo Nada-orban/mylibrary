a073632992b1f0542dd8d5fbf38ea65bd79a8b34
nada <nadasamir9334@gmail.com>
2023-07-24 23:20:43 -0400

    save some edit

--- mylibrary/settings.py
+++ mylibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-91nsqxd@jey6fqb%(*=ba9h-!(*8qgtgcbw09gvv)y0likv^t!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -77,10 +77,10 @@ WSGI_APPLICATION = 'mylibrary.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
